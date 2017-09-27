7315d28de7533b0d288266a34439b9a42f8da634
zhaoyang <zhaoyang@corp-ci.com>
2017-09-27 18:02:31 +0800

    update classify

--- src/views/classify.vue
+++ src/views/classify.vue
@@ -6,56 +6,81 @@
                 <div class="arrow" @click="jumpout()"></div>
                 <div class="textdiv"><text class="text">分类</text></div>
             </div>
-            <OTabs :value="tab4" @input="tabsChange4" :tabIndex="tabIndex4" class="tabs">
-                <OTab label="房屋租赁" value="one">
+            <OTabsVertical :value="tab4" @input="tabsChange4" :tabIndex="tabIndex4" class="tabs">
+                <OTabVertical label="房屋租赁" value="one">
                    <TabContent></TabContent>
-                    <!--<text>已发货...</text>-->
-                </OTab>
-                <OTab label="技能服务" value="two">
+                </OTabVertical>
+                <OTabVertical label="技能服务" value="two">
                   <TabContent></TabContent>
-                </OTab>
-                <OTab label="移动电话" value="three">
+                </OTabVertical>
+                <OTabVertical label="移动电话" value="three">
                   <TabContent></TabContent>
-                </OTab>
-                <OTab label="女士服装" value="four">
-                    <text>未发货2...</text>
-                </OTab>
-                <OTab label="个护美妆" value="fIve">
-                    <text>未发货2...</text>
-                </OTab>
-                <OTab label="箱包" value="six">
-                    <text>未发货2...</text>
-                </OTab>
-                <OTab label="运动户外" value="seven">
-                    <text>未发货2...</text>
-                </OTab>
-                <OTab label="家用电器" value="eight">
-                    <text>未发货2...</text>
-                </OTab>
-                <OTab label="玩具乐器" value="nine">
-                    <text>未发货2...</text>
-                </OTab>
-                <OTab label="珠宝首饰" value="ten">
-                    <text>未发货2...</text>
-                </OTab>
-                <OTab label="技能服务" value="eleven">
-                    <text>未发货2...</text>
-                </OTab>
-            </OTabs>
+                </OTabVertical>
+                <OTabVertical label="女士服装" value="four">
+                  <TabContent></TabContent>
+                </OTabVertical>
+                <OTabVertical label="个护美妆" value="fIve">
+                  <TabContent></TabContent>
+                </OTabVertical>
+                <OTabVertical label="箱包" value="six">
+                  <TabContent></TabContent>
+                </OTabVertical>
+                <OTabVertical label="运动户外" value="seven">
+                  <TabContent></TabContent>
+                </OTabVertical>
+                <OTabVertical label="家用电器" value="eight">
+                  <TabContent></TabContent>
+                </OTabVertical>
+                <OTabVertical label="玩具乐器" value="nine">
+                  <TabContent></TabContent>
+                </OTabVertical>
+                <OTabVertical label="珠宝首饰" value="ten">
+                  <TabContent></TabContent>
+                </OTabVertical>
+                <OTabVertical label="女士鞋靴" value="eleven">
+                  <TabContent></TabContent>
+                </OTabVertical>
+              <OTabVertical label="动漫/周边" value="twelve">
+                <TabContent></TabContent>
+              </OTabVertical>
+              <OTabVertical label="汽车/用品" value="thirteen">
+                <TabContent></TabContent>
+              </OTabVertical>
+              <OTabVertical label="游戏设备" value="fourteen">
+                <TabContent></TabContent>
+              </OTabVertical>
+              <OTabVertical label="居家日用" value="fifteen">
+                <TabContent></TabContent>
+              </OTabVertical>
+              <OTabVertical label="宠物/用品" value="sixteen">
+                <TabContent></TabContent>
+              </OTabVertical>
+              <OTabVertical label="男士服装" value="seventeen">
+                <TabContent></TabContent>
+              </OTabVertical>
+              <OTabVertical label="钟表眼镜" value="eighteen">
+                <TabContent></TabContent>
+              </OTabVertical>
+              <OTabVertical label="办公用品" value="nineteen">
+                <TabContent></TabContent>
+              </OTabVertical>
+              <OTabVertical label="服饰配件" value="twenty">
+                <TabContent></TabContent>
+              </OTabVertical>
+            </OTabsVertical>
         </ONavPage>
     </div>
 </template>
 <script>
   import { ONavPage } from 'iweex'
-  //, OTab, OTabs
-  import OTab from '../components/O-Tab.vue'
-  import OTabs from '../components/O-Tabs.vue'
+  import OTabVertical from '../components/O-TabVertical.vue'
+  import OTabsVertical from '../components/O-TabsVertical.vue'
   import TabContent from './TabContent.vue'
   export default {
     components: {
       ONavPage,
-      OTab,
-      OTabs,
+      OTabVertical,
+      OTabsVertical,
       TabContent
     },
     data () {
